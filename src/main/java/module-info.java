a3122a2f3c2874650d710de41b3d65c71dcda1e4
MSKLN6 <daan.bussels@student.uhasselt.be>
2021-12-23 23:19:57 +0100

    Versie Daan 23:20

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module be.fiiw.exomeda {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens be.fiiw.exomeda to javafx.fxml;
     exports be.fiiw.exomeda;
